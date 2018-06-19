0f60e48b2216b9f1dfa1b89d5c3f5a7a0673840d
Ryubu Hosoki <hosoki.r.aa@m.titech.ac.jp>
2018-06-19 17:37:39 +0900

    finish

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i;
+    int sum = 0;
+    for(i = 0; i < n; i++){
+        sum = sum + (A[i] / m) + 1;
+    }
+    return (long long int) sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 1;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+  printf("%d\n", lb);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,26 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i;
+    int sum = 0;
+    int cnt = 1;
+    for(i = 0; i < n; i++){
+        if(sum + A[i] <= m){
+            sum = sum + A[i];
+        }
+        else {
+            if(A[i] > m){
+                cnt = k + 1;
+            }
+            else {
+            sum = A[i];
+            cnt = cnt + 1;
+            }
+        }
+    }
+    return (long long int) cnt <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +31,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 1;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", ub);
   return 0;
 }
